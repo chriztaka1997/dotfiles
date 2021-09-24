302e421eb846d4a6ec9ba14fa3c5944b7ac1e003
Christopher Imantaka <30333920+chriztaka1997@users.noreply.github.com>
2021-09-23 17:38:34 -0700

    Added github shortcut

--- zsh/shortcut.sh
+++ zsh/shortcut.sh
@@ -20,6 +20,7 @@ instagram="instagram.com/direct/inbox"
 mangakakalot="https://mangakakalot.com/bookmark"
 yt="youtube.com"
 google="https://www.google.com/"
+gith="https://github.com/chriztaka1997"
 
 # Chrome shortcut
 alias chrome="open -na Google\ Chrome --args --new-window"
@@ -27,3 +28,4 @@ alias search="chrome $google"
 alias insta="chrome $instagram"
 alias manga="chrome $mangakakalot"
 alias youtube="chrome $yt"
+alias github="chrome $gith"
